bd1b2102935e2c95ec2235d215e6bf938bbee076
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-02-27 15:56:14 +0200

    First commit

---
+++ SpisykOtImena/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpisykOtImena
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine().Split().ToList();
+            int i = 0;
+            for (i = 0; i < names.Count-1; i++)
+            {
+                Console.Write($"{names[i]}; ");
+            }
+            Console.Write(names[i]);
+            Console.WriteLine();
+            //ne se poluchi sys string.Join
+        }
+    }
+}
---
+++ VuvejdaneNaSpisykOtKonzolataNaEdinRed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VuvejdaneNaSpisykOtKonzolataNaEdinRed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums =Console.ReadLine().Split().Select(int.Parse).ToList();
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Console.WriteLine($"nums[{i}] = {nums[i]}");
+            }
+        }
+    }
+}
